aa9986e42b0f5fe0e828280eab50fb922663ae4c
Tscodes70 <sck.timothy@gmail.com>
2023-09-14 16:50:04 +0800

    Reimplemented SA, extracted different factors (Province,Category,Subcategory,Total Reviews,Average Rating

--- analyze.py
+++ analyze.py
@@ -1,105 +1,98 @@
 import nltk
-import csv
+import csv, pandas as pd
+from nltk.tokenize import word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Prepare VADER pre-trained model
+nltk.download('punkt')
 nltk.download('vader_lexicon')
 
 FILENAME = "data.csv"
 ENCODING = "utf8"
-PSENTIMENT = 0.05
-NSENTIMENT = -0.05
 
-# def totalRows(filename):
-#     with open(filename, encoding=ENCODING) as f:
-#         print(sum(1 for line in f))
+OUTPUTFILE = 'outputdata.csv'
+# PSENTIMENT = 0.05
+# NSENTIMENT = -0.05
 
-def getUniqueHotelTuple(hotelNameList,hotelPcodeList):
-    """
-    Make NameList & PostalList unique and turns it into a tuple
-    while preserving the order.
-    """
-    # Create empty lists to maintain the order for unique names and codes
-    unique_hotel_names = []
-    unique_hotel_codes = []
+#CSV Headers
+HOTELNAME =  "name"
+HOTELPROVINCE = "province"
+HOTELPOSTALCODE = "postalCode"
+HOTELPCATEGORY = "categories"
+HOTELSCATEGORY = "primaryCategories"
+HOTELREVIEWS = "reviews.text"
+HOTELRATING = "reviews.rating"
 
-    # Iterate through both lists, adding unique pairs to the new lists
-    for name, code in zip(hotelNameList, hotelPcodeList):
-        if (name, code) not in zip(unique_hotel_names, unique_hotel_codes):
-            unique_hotel_names.append(name)
-            unique_hotel_codes.append(code)
-    
-    return tuple(zip(unique_hotel_names, unique_hotel_codes))
+RATINGMAX = 5
 
-
-def getSortedData(filename):
-    """
-    This functions takes in data from a csv and sorts
-    by grouping each hotel's reviews to its own postal code.
-    """
-    hotelPcodeList = []
-    hotelNameList =[]
-    reviewList = []
-
-    with open(filename, encoding=ENCODING) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            hotelPcodeList.append(row['postalCode'])
-            hotelNameList.append(row['name'])
-            reviewList.append(row['reviews.text'])
-        
-    hotelUniqueTuple = getUniqueHotelTuple(hotelNameList,hotelPcodeList)
-    
-    return [[hotel[0], hotel[1],[review for i, review in enumerate(reviewList) if hotelPcodeList[i] == hotel[1]]] for hotel in hotelUniqueTuple]
-
-
-def nltkAnalyzer(sortedData):
+def nltkAnalyzer(processedData):
     """
     This functions takes in a List of text reviews
     and analyzes them using the VADER Sentiment Analysis
     which is using the Natural Language ToolKit(NLTK)
-    and prints the sentiment scores of the reviews
+    and prints the compound sentiment scores of the reviews
     70% Accuracy
     """
-    vaderAnalyzer = SentimentIntensityAnalyzer()
-    posSentCounter = 0
-    negSentCounter = 0
-    neuSentCounter = 0
+     # Initialize the sentiment analyzer
+    sia = SentimentIntensityAnalyzer()
 
-    # Analyze each review
-    for hotelData in sortedData:
-        sentiment_scores = vaderAnalyzer.polarity_scores(hotelData)
-        #print(f"Review: {review}")
-        #print(f"Sentiment Scores: {sentiment_scores}")
+    # Tokenize and analyze the reviews
+    processedData['Sentiment Scores'] = processedData['Review'].apply(lambda x: sia.polarity_scores(x))
 
-        # Determine sentiment by calculating compound score
-        compound_score = sentiment_scores['compound']
-        if compound_score >= PSENTIMENT:
-            sentiment = "Positive"
-            posSentCounter+=1
+    # Extract sentiment scores
+    processedData['Compound Sentiment'] = processedData['Sentiment Scores'].apply(lambda x: x['compound'])
 
-        elif compound_score <= NSENTIMENT:
-            sentiment = "Negative"
-            negSentCounter+=1
-        else:
-            sentiment = "Neutral"
-            neuSentCounter+=1
-
-        #print(f"Sentiment: {sentiment}\n")
+    outputCsv(processedData[['Hotel Name', 'Province', 'Postal','Category','SubCategory', 'Compound Sentiment', 'Total Reviews', 'Average Rating']])
     
-    print(f"Positive: {posSentCounter} Negative: {negSentCounter} Neutral: {neuSentCounter}")
-    print("====================================================\n\n\n")
+    # Cannot output reviews in csv as excel has char limit of 32k
+    # outputCsv(processedData[['Hotel Name', 'Province', 'Postal', 'Review', 'Compound Sentiment', 'Total Reviews']])
 
-def printData(sortedData):
-    for hotelData in sortedData:
-        print("====================================================")
-        print(f"Hotel: {hotelData[0]}")
-        print(f"Hotel Code: {hotelData[1]}")
-        nltkAnalyzer(hotelData[2])
-            
+def processDataFromCsv(csvFilename):
+    HotelName,HotelProvince,HotelPostal,HotelPrimaryCategory,HotelSubCategory,HotelReviews,HotelRating = [], [], [], [], [], [],[]
+
+    with open(csvFilename, encoding=ENCODING) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            HotelName.append(row[HOTELNAME])
+            HotelProvince.append(row[HOTELPROVINCE])
+            HotelPostal.append(row[HOTELPOSTALCODE])
+            HotelPrimaryCategory.append(row[HOTELPCATEGORY])
+            HotelSubCategory.append(row[HOTELSCATEGORY])
+            HotelReviews.append(row[HOTELREVIEWS])
+            HotelRating.append(float(row[HOTELRATING]))
+
+    data = {
+    'Hotel Name': HotelName,
+    'Province': HotelProvince,
+    'Postal':HotelPostal,
+    'Category':HotelPrimaryCategory,
+    'SubCategory':HotelSubCategory,
+    'Review': HotelReviews,
+    'Average Rating': HotelRating
+    }
+    df = pd.DataFrame(data)
+    """
+        Used chatgpt to generate {'Review': lambda x: '<SPLIT> '.join(x), 'Average Rating': 'mean'}
+        Reason: Encountering alot of errors & unsure of the formatting when applying 2 functions 
+        while only resetting index once to dataframe.
+
+        Original statement pre-chatgpt commented.
+
+    """
+    groupedData = df.groupby(['Hotel Name', 'Province', 'Postal', 'Category', 'SubCategory']).agg({'Review': lambda x: '<SPLIT> '.join(x), 'Average Rating': 'mean'}).reset_index()
+    #groupedData = df.groupby(['Hotel Name', 'Province','Postal','Category','SubCategory'])['Review'].agg(lambda x: '<SPLIT> '.join(x)).reset_index()
+    
+    # Formatting for output csv
+    groupedData['Average Rating'] = groupedData['Average Rating'].round(2)
+    groupedData['Total Reviews'] = groupedData['Review'].apply(lambda x: len(x.split('<SPLIT> ')))
+
+    nltkAnalyzer(groupedData)
+
+def outputCsv(data):
+    data.to_csv(OUTPUTFILE)
+    
 def main():
-    printData(getSortedData(FILENAME))
-
+    processDataFromCsv(FILENAME)
     
 main()
 
