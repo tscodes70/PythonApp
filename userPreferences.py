3cb6b1d338a5fe98e9b4057339b2b40c35e69b1b
piconjoz <55548225+piconjoz@users.noreply.github.com>
2023-10-05 17:14:17 +0000

    create a dashboard based on new schematics

--- userPreferences.py
+++ userPreferences.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_bootstrap import Bootstrap
 import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
 #import numpy as np
 
 '''
@@ -27,7 +29,23 @@ def openCsv():
 
 @app.route('/')
 def navigation():
-    return render_template('userDashboard.html')
+    # Load the CSV data into a Pandas DataFrame
+    df = pd.read_csv('NEWOUTPUT.csv')
+
+    # Assuming the CSV has a 'Category' column, get the top 4 values
+    top_categories = df['Category'].value_counts().nlargest(4)
+
+    # Create the Plotly pie chart
+    pie_chart = go.Figure(data=[go.Pie(labels=top_categories.index, values=top_categories)])
+
+    # Convert the chart to an HTML div
+    pie_chart_div = pie_chart.to_html(full_html=False)
+    
+    ratingColumn = 'Average Rating'
+    histogram = px.histogram(df, x=ratingColumn, title=f'Histogram of Average Rating')
+    histogram_div = histogram.to_html()
+    return render_template('userDashboard.html', pie_chart_div=pie_chart_div, histogram_div=histogram_div)
+    #return render_template('dashboard2.html', pie_chart_div=pie_chart_div, histogram_div=histogram_div)
 
 @app.route('/table', methods=("POST", "GET"))
 def index():
