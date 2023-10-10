4f7166fbb4f0d949d09f7d720b8156f5d056f043
faziera22 <fazierarazale@gmail.com>
2023-10-10 23:13:40 +0800

    added map but not rly working yet (takes very long to load)

--- userPreferences.py
+++ userPreferences.py
@@ -10,6 +10,13 @@ from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
 from flask import json
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import contextily as ctx 
+import geopandas as gpd 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import geopandas as gpd 
 #import numpy as np
 
 '''
@@ -36,7 +43,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # takes data from outputdata.csv and loads it into a pandas dataframe
-df = pd.read_csv('NEWOUTPUT.csv', index_col=0)
+df = pd.read_csv('analyzedhotels_10-Oct.csv', index_col=0)
+
+# Load a geospatial dataset of the USA
+gdf = gpd.read_file(r'C:\Users\legendary22\Documents\GitHub\PythonApp\tl_2022_us_state\tl_2022_us_state.shp')
 
 # Route to get unique provinces for the dropdown
 @app.route('/get_provinces')
@@ -57,10 +67,9 @@ def filter_data():
     filtered_data_html = filtered_data.to_html()
     return filtered_data_html
 
-@app.route('/piechart', methods=['POST'])
 def piechart():
     # Assuming the CSV has a 'Category' column, get the top 4 values
-    top_categories = df['Category'].value_counts().nlargest(4)
+    top_categories = df['categories'].value_counts().nlargest(4)
     # Create the Plotly pie chart
     pie_chart = go.Figure(data=[go.Pie(labels=top_categories.index, values=top_categories)])
     # Convert the chart to an HTML div
@@ -68,19 +77,20 @@ def piechart():
 
     return pie_chart_div
 
-@app.route('/histogram', methods=['POST'])
-def histogram():
-    #wordcloud = wordcloud()
-    selected_hotel = request.form['hotelName-dropdown-histogram']
+@app.route('/filtered_charts', methods=['POST'])
+def filtered_charts():
+    pie_chart_div = piechart()
+    selected_hotel = request.form['hotelName-dropdown']
     
+    # histogram
     if selected_hotel:
-        filtered_data = df[df['Hotel Name'] == selected_hotel]
-        data = filtered_data['Average Rating'].astype(float)
+        histogram_filtered_data = df[df['name'] == selected_hotel]
+        histogram_data = histogram_filtered_data['average.rating'].astype(float)
     else:
-        data = df['Average Rating'].astype(float)
+        histogram_data = df['average.rating'].astype(float)
 
     # Create a histogram figure using go.Figure
-    histogram = go.Figure(data=[go.Histogram(x=data)])
+    histogram = go.Figure(data=[go.Histogram(x=histogram_data)])
     histogram.update_layout(
         title=f'Histogram of Average Rating',
         xaxis_title='Average Rating',
@@ -88,55 +98,69 @@ def histogram():
     )
     histogram_div = histogram.to_html()
     
-    return render_template('userDashboard.html', histogram_div=histogram_div, hotelName=selected_hotel)
-
-@app.route('/wordcloud', methods=['POST'])
-def wordcloud():
-    pie_chart_div = piechart()
-    #histogram_div = histogram()
-
-    selected_hotel = request.form['hotelName-dropdown']
-    hotel_name = df['Hotel Name'].unique()
-    heading = "Word Cloud on the reviews for " + selected_hotel
+    # wordcloud
+    hotel_name = df['name'].unique()
+    wordcloud_heading = "Word Cloud on the reviews for " + selected_hotel
+    histogram_heading = "Histogram on the ratings for " + selected_hotel
 
     if selected_hotel:
-        filtered_data = df[df['Hotel Name'] == selected_hotel]
-        text = ' '.join(filtered_data['Review Summary'].astype(str))
+        wordcloud_filtered_data = df[df['name'] == selected_hotel]
+        wordcloud_data = ' '.join(wordcloud_filtered_data['reviews.summary'].astype(str))
     else:
-        text = ' '.join(df['Review Summary'].astype(str))
-    
+        wordcloud_data = ' '.join(df['reviews.summary'].astype(str))
+
     # Generate the word cloud
     wordcloud = WordCloud(width=800, height=400)
-    wordcloud.generate(text)
-
-    # Render the word cloud as a base64-encoded image
+    wordcloud.generate(wordcloud_data)
     img_buffer = BytesIO()
-    plt.figure(figsize=(8, 4))
-    plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.savefig(img_buffer, format='png')
     img_data = base64.b64encode(img_buffer.getvalue()).decode('utf-8')
 
-    return render_template('userDashboard.html', hotelNames= hotel_name, wordcloud_heading=heading, img_data=img_data, hotelName=selected_hotel, pie_chart_div=pie_chart_div)
+    return render_template('userDashboard.html', img_data=img_data, hotelNames= hotel_name, histogram_heading=histogram_heading,                        wordcloud_heading=wordcloud_heading, histogram_div=histogram_div, hotelName=selected_hotel, pie_chart_div=pie_chart_div)
+
+def map():
+    df['reviews.total'] = df['reviews.total'].astype(str)
+    # Merge the data based on 'Province' and calculate the total number of reviews
+    merged_data = gdf.merge(df, left_on='STUSPS', right_on='province', how='left')
+
+    # Normalize the data if needed
+    # Here, we are assuming you have a column 'Total Reviews' in your CSV data
+    # You may want to scale the data to fit the color scale properly
+
+    # Create a chloropleth map using Plotly Express
+    fig = px.choropleth(merged_data, 
+                        geojson=gdf.geometry, 
+                        locations=merged_data.index, 
+                        color='reviews.total',
+                        hover_name='STUSPS')
+
+    # Convert the map to HTML
+    map_div = fig.to_html(full_html=False)
+    return render_template('testfaz.html', map_div=map_div)
 
 @app.route('/', methods=['GET', 'POST'])
 def navigation():
     pie_chart_div = piechart()
-    
+    #map_div = map()
+    histogram_heading = "Histogram"
     wordcloud_heading = "Word Cloud"
 
-    hotel_name = df['Hotel Name'].unique()
-
-    selected_province = request.args.get('province')
-
-    if selected_province:
-        filtered_data = df[df['Province'] == selected_province]
-        text = ' '.join(filtered_data['Review Summary'].astype(str))
-    else:
-        text = ' '.join(df['Review Summary'].astype(str))
-        
-    # Replace 'column_name' with the name of the CSV column you want to create a word cloud from
-    # text = ' '.join(df['Review Summary'].astype(str))
+    hotel_name = df['name'].unique()
+    
+    # Create a histogram figure using go.Figure
+    histogram_data = df['average.rating'].astype(float)
+    histogram_data = df['average.rating'].astype(float)
+    histogram = go.Figure(data=[go.Histogram(x=histogram_data)])
+    histogram.update_layout(
+        title=f'Histogram of Average Rating',
+        xaxis_title='Average Rating',
+        yaxis_title='Count'
+    )
+    histogram_div = histogram.to_html()
+    
+    # Generate the word cloud
+    text = ' '.join(df['reviews.summary'].astype(str))
 
     # Generate the word cloud
     wordcloud = WordCloud(width=800, height=400)
@@ -150,7 +174,8 @@ def navigation():
     plt.savefig(img_buffer, format='png')
     img_data = base64.b64encode(img_buffer.getvalue()).decode('utf-8')
     
-    return render_template('userDashboard.html', wordcloud_heading = wordcloud_heading, hotelNames=hotel_name, pie_chart_div=pie_chart_div, img_data=img_data)
+    #return render_template('userDashboard.html', pie_chart_div=pie_chart_div, histogram_heading=histogram_heading, wordcloud_heading=wordcloud_heading)
+    return render_template('userDashboard.html',  histogram_heading=histogram_heading, histogram_div=histogram_div, wordcloud_heading = wordcloud_heading, hotelNames=hotel_name, pie_chart_div=pie_chart_div, img_data=img_data)
     #return render_template('dashboard2.html', pie_chart_div=pie_chart_div, histogram_div=histogram_div)
 
 # import csv
